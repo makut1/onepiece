1ce7cfa259a77cfdb80939b40bef599a21fac83d
mayihu <mayihu@qiyi.com>
2021-09-24 15:07:03 +0800

    去除moment js

--- src/main.js
+++ src/main.js
@@ -6,13 +6,13 @@ import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
 
 import axios from 'axios'
-import moment  from 'moment'
+// import moment  from 'moment'
 
 
-Vue.filter('moment', function (value, formatString) {
-  formatString = formatString || 'YYYY-MM-DD HH:mm:ss';
-  return moment(value).format("YYYY-MM-DD"); // value可以是普通日期 20170723
-});
+// Vue.filter('moment', function (value, formatString) {
+//   formatString = formatString || 'YYYY-MM-DD HH:mm:ss';
+//   return moment(value).format("YYYY-MM-DD"); // value可以是普通日期 20170723
+// });
 
 Vue.prototype.$axios = axios
 
